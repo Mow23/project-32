6bee59778a04d0cc675976822ddce0dbd1ff746d
Mow23 <drsatyenjoshi@gmail.com>
2021-09-29 16:22:16 +0530

    hi

---
+++ Slingshot.js
@@ -0,0 +1,46 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.3,
+            length: 5
+        }
+       /* this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");*/
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+      /*  image(this.image1, 200, 25);
+        image(this.image2, 170, 25);*/
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push ();
+            if (pointA.x<200){
+                strokeWeight(3);
+                stroke("pink");
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+          //  image(this.image3, pointA.x-30, pointA.y-20, 15, 30);
+            } else {
+                strokeWeight(6);
+                stroke("pink");
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+         //   image(this.image3, pointA.x-30, pointA.y-20, 15, 30);
+            }
+            pop ();
+        }
+        
+    }
+    
+}
--- sketch.js
+++ sketch.js
@@ -1,1 +1,66 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
+var engine, world;
+var box1, platform, slingShot;
+
+function preload() {
+   // backgroundImg = loadImage("sprites/bg.png");
+   polygonImg = loadImage("polygon.png");
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    stand = new Ground(850,300,250,20);
+
+    box1 = new Box(820,270,30,40);
+    box2 = new Box(850,270,30,40);
+    box3 = new Box(880,270,30,40);
+    box4 = new Box(835,230,30,40);
+    box5 = new Box(865,230,30,40);
+    box6 = new Box(850,190,30,40);
+
+    polygon = Bodies.circle(500,200,20);
+    World.add(world,polygon);
+
+  slingshot = new SlingShot(this.polygon,{x:100, y:200});
+}
+
+function draw(){
+  background("black");
+    Engine.update(engine);
+    strokeWeight(4);
+    ground.display();
+    fill ("orange");
+    box1.display();
+    box2.display();
+    box3.display();
+    fill ("yellow");
+    box4.display();
+    box5.display();
+    fill ("pink");
+    box6.display();
+    stand.display();
+    fill("blue")
+    imageMode(CENTER);
+    image(polygonImg,polygon.position.x,polygon.position.y, 40,40);
+    slingshot.display();
+   
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(polygon, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
